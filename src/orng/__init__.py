5cef596e64054b5c650cbc0c0a106c619cda7375
Michael J. Williams <michaeljw1@googlemail.com>
2026-02-26 19:14:20 +0000

    ENH: expose functional backend

--- src/orng/__init__.py
+++ src/orng/__init__.py
@@ -5,6 +5,7 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
+from .functional import create_functional_backend
 from .orng import ArrayRNG
 
-__all__ = ["ArrayRNG", "__version__"]
+__all__ = ["ArrayRNG", "create_functional_backend", "__version__"]
